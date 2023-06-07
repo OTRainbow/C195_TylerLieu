849693801ffd402e2cd9c231e2faf0f35e9faa0c
Tyler Lieu <tylerlieu0718@gmail.com>
2023-06-07 07:42:33 -0500

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.tylerlieu.c195 {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.tylerlieu.c195 to javafx.fxml;
+    exports com.tylerlieu.c195;
+}
